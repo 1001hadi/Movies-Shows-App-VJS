8a27caad23f03db8d888f03e227ec095c961a9a1
1001hadi <hadi.jaf.1001@gmail.com>
2023-09-21 10:31:42 -0600

    Popular movies / tv shows fetched from API

---
+++ js/script.js
@@ -0,0 +1,131 @@
+// API => "6796e7b256c8b9ca81e5d0655511ed16"
+
+const global = {
+  currPage: window.location.pathname,
+};
+
+// display popular movies
+async function displayPopularMovies() {
+  const { results } = await fetchAPIData("movie/popular");
+  results.forEach((movie) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `
+    <a href="movie-details.html?id=${movie.id}">
+      ${
+        movie.poster_path
+          ? `<img
+      src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+      class="card-img-top"
+      alt=""${movie.title}"
+    />`
+          : `<img
+    src="../images/no-image.jpg"
+    class="card-img-top"
+    alt="${movie.title}"
+  />`
+      }
+    </a>
+    <div class="card-body">
+      <h5 class="card-title">${movie.title}</h5>
+      <p class="card-text">
+        <small class="text-muted">Release Date: ${movie.release_date}</small>
+      </p>
+    </div>
+  `;
+
+    document.querySelector("#popular-movies").appendChild(div);
+  });
+}
+
+// display popular TV shows
+async function displayPopularTvShows() {
+  const { results } = await fetchAPIData("tv/popular");
+  results.forEach((show) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `
+      <a href="movie-details.html?id=${show.id}">
+        ${
+          show.poster_path
+            ? `<img
+        src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+        class="card-img-top"
+        alt=""${show.name}"
+      />`
+            : `<img
+      src="../images/no-image.jpg"
+      class="card-img-top"
+      alt="${show.name}"
+    />`
+        }
+      </a>
+      <div class="card-body">
+        <h5 class="card-title">${show.name}</h5>
+        <p class="card-text">
+          <small class="text-muted">Release Date: ${show.first_air_date}</small>
+        </p>
+      </div>
+    `;
+
+    document.querySelector("#popular-shows").appendChild(div);
+  });
+}
+
+// Fetch Data (tmdb)
+async function fetchAPIData(endpoint) {
+  showSpinner();
+  const API_KEY = "6796e7b256c8b9ca81e5d0655511ed16";
+  const API_URL = "https://api.themoviedb.org/3/";
+
+  const response = await fetch(
+    `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
+  );
+
+  const data = await response.json();
+  hideSpinner();
+  return data;
+}
+
+// Adding speinner
+function showSpinner() {
+  document.querySelector(".spinner").classList.add("show");
+}
+function hideSpinner() {
+  document.querySelector(".spinner").classList.remove("show");
+}
+
+// active Link
+function activeLinkHover() {
+  const links = document.querySelectorAll(".nav-link");
+  links.forEach((link) => {
+    if (link.getAttribute("href") === global.currPage) {
+      link.classList.add("active");
+    }
+  });
+}
+
+// Init App
+function init() {
+  switch (global.currPage) {
+    case "/":
+    case "/index.html":
+      displayPopularMovies();
+      break;
+    case "/shows.html":
+      displayPopularTvShows();
+      break;
+    case "/movie-details.html":
+      console.log("Movies");
+      break;
+    case "/tv-details.html":
+      console.log("TV");
+      break;
+    case "/search.html":
+      console.log("Search");
+      break;
+  }
+  activeLinkHover();
+}
+
+document.addEventListener("DOMContentLoaded", init);
